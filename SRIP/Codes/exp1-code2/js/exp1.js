dfc77b85264777d6677e134fa6b199f75bd5ae25
K-Kavitha-Kiran <48920405+K-Kavitha-Kiran@users.noreply.github.com>
2019-06-05 22:28:12 +0530

    Update exp1.js
    modified js file

--- SRIP/Codes/exp1-code2/js/exp1.js
+++ SRIP/Codes/exp1-code2/js/exp1.js
@@ -1,7 +1,7 @@
 function xa() {
   var elem = document.getElementById("xabox");   
   var pos = 167;
-  var id = setInterval(frame, 150);
+  var id = setInterval(frame, 190);
   function frame() {
     if (pos == 112) {
       clearInterval(id);
@@ -14,7 +14,7 @@ function xa() {
 function xb() {
   var elem = document.getElementById("xbbox");   
   var pos = 222;
-  var id = setInterval(frame, 150);
+  var id = setInterval(frame, 190);
   function frame() {
     if (pos == 167) {
       clearInterval(id);
@@ -26,10 +26,10 @@ function xb() {
 }
 function xc() {
   var elem = document.getElementById("xcbox");   
-  var pos = 260;
-  var id = setInterval(frame, 150);
+  var pos = 261;
+  var id = setInterval(frame, 190);
   function frame() {
-    if (pos == 205) {
+    if (pos == 206) {
       clearInterval(id);
     } else {
       pos--; 
@@ -40,7 +40,7 @@ function xc() {
     function ya() {
   var elem = document.getElementById("yabox");   
   var pos = 476;
-  var id = setInterval(frame, 150);
+  var id = setInterval(frame, 190);
   function frame() {
     if (pos == 531) {
       clearInterval(id);
@@ -53,7 +53,7 @@ function xc() {
 function yb() {
   var elem = document.getElementById("ybbox");   
   var pos = 438;
-  var id = setInterval(frame, 150);
+  var id = setInterval(frame, 190);
   function frame() {
     if (pos == 493) {
       clearInterval(id);
@@ -65,14 +65,79 @@ function yb() {
 }
 function yc() {
   var elem = document.getElementById("ycbox");   
-  var pos = 300;
-  var id = setInterval(frame, 150);
+  var pos = 330;
+  var id = setInterval(frame, 190);
   function frame() {
-    if (pos == 355) {
+    if (pos == 385) {
       clearInterval(id);
     } else {
       pos++; 
       elem.style.top = pos + 'px'; 
          }
+  }
+}
+    function xd() {
+  var elem = document.getElementById("down");   
+  var pos = 369;
+  var id = setInterval(frame, 190);
+  function frame() {
+    if (pos == 314) {
+      clearInterval(id);
+    } else {
+      pos--; 
+      elem.style.top = pos + 'px'; 
+         }
+  }
+}
+    function yd() {
+  var elem = document.getElementById("up");   
+  var pos = 286;
+  var id = setInterval(frame, 190);
+  function frame() {
+    if (pos == 341) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.top = pos + 'px'; 
+         }
+  }
+}
+	 function w1() {
+  var elem = document.getElementById("dot3");   
+  var pos = 555;
+  var id = setInterval(frame, 130);
+  function frame() {
+    if (pos == 640) {
+      clearInterval(id);
+    } else {
+      pos++; 
+	elem.style.left = pos + 'px'; 
+       }
+  }
+}
+	function w2() {
+  var elem = document.getElementById("dot4");   
+  var pos = 760;
+  var id = setInterval(frame, 130);
+  function frame() {
+    if (pos == 675) {
+      clearInterval(id);
+    } else {
+      pos--; 
+	elem.style.left = pos + 'px'; 
+       }
+  }
+}
+	function w3() {
+  var elem = document.getElementById("dot1");   
+  var pos = 363;
+  var id = setInterval(frame, 410);
+  function frame() {
+    if (pos == 333) {
+      clearInterval(id);
+    } else {
+      pos--; 
+	elem.style.top = pos + 'px'; 
+       }
   }
 }
