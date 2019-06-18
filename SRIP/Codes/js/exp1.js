a1d9f3067b966e6eb1043bb4d790d4b1a8c8fc15
K-Kavitha-Kiran <48920405+K-Kavitha-Kiran@users.noreply.github.com>
2019-06-18 20:24:01 +0530

    Update and rename SRIP/Codes/exp1-code2/js/exp1.js to SRIP/Codes/js/exp1.js
    modified js file

--- SRIP/Codes/js/exp1.js
+++ SRIP/Codes/js/exp1.js
@@ -1,7 +1,19 @@
-function xa() {
+function move() {
+		xa();
+		xb();
+		xc();
+		xd();
+		ya();
+		yb();
+		yc();
+		yd();
+		w1();
+		w2();
+	}
+	function xa() {
   var elem = document.getElementById("xabox");   
   var pos = 167;
-  var id = setInterval(frame, 150);
+  var id = setInterval(frame, 200);
   function frame() {
     if (pos == 112) {
       clearInterval(id);
@@ -14,7 +26,7 @@ function xa() {
 function xb() {
   var elem = document.getElementById("xbbox");   
   var pos = 222;
-  var id = setInterval(frame, 150);
+  var id = setInterval(frame, 200);
   function frame() {
     if (pos == 167) {
       clearInterval(id);
@@ -27,7 +39,7 @@ function xb() {
 function xc() {
   var elem = document.getElementById("xcbox");   
   var pos = 260;
-  var id = setInterval(frame, 150);
+  var id = setInterval(frame, 200);
   function frame() {
     if (pos == 205) {
       clearInterval(id);
@@ -40,7 +52,7 @@ function xc() {
     function ya() {
   var elem = document.getElementById("yabox");   
   var pos = 476;
-  var id = setInterval(frame, 150);
+  var id = setInterval(frame, 200);
   function frame() {
     if (pos == 531) {
       clearInterval(id);
@@ -53,7 +65,7 @@ function xc() {
 function yb() {
   var elem = document.getElementById("ybbox");   
   var pos = 438;
-  var id = setInterval(frame, 150);
+  var id = setInterval(frame, 200);
   function frame() {
     if (pos == 493) {
       clearInterval(id);
@@ -66,7 +78,7 @@ function yb() {
 function yc() {
   var elem = document.getElementById("ycbox");   
   var pos = 340;
-  var id = setInterval(frame, 150);
+  var id = setInterval(frame, 200);
   function frame() {
     if (pos == 395) {
       clearInterval(id);
@@ -79,7 +91,7 @@ function yc() {
 	function xd() {
   var elem = document.getElementById("xdt");   
   var pos = 360;
-  var id = setInterval(frame, 150);
+  var id = setInterval(frame, 200);
   function frame() {
     if (pos == 305) {
       clearInterval(id);
@@ -92,7 +104,7 @@ function yc() {
 	function yd() {
   var elem = document.getElementById("ydt");   
   var pos = 290;
-  var id = setInterval(frame, 150);
+  var id = setInterval(frame, 200);
   function frame() {
     if (pos == 345) {
       clearInterval(id);
@@ -106,7 +118,7 @@ function yc() {
 	function w1() {
   var elem = document.getElementById("dot1");   
   var pos = 393;
- var id = setInterval(frame, 160);
+ var id = setInterval(frame, 213);
   function frame() {
     if (pos == 341) {
       clearInterval(id);
@@ -119,7 +131,7 @@ function yc() {
 	function w2() {
   var elem = document.getElementById("dot2");   
   var pos = 320;
- var id = setInterval(frame, 265);
+ var id = setInterval(frame, 353);
   function frame() {
     if (pos == 351) {
       clearInterval(id);
